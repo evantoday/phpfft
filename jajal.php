4b499992958d864420ba80e5746d539512778032
Evan Nur R <andik.cb6@gmail.com>
2019-01-28 11:57:18 +0000

    dadi mentah

--- jajal.php
+++ jajal.php
@@ -1,5 +1,6 @@
 
 <?php
+error_reporting(0);
 set_time_limit(0);
 date_default_timezone_set('UTC');
 require __DIR__.'/vendor/autoload.php';
@@ -25,7 +26,7 @@ if($i == 3){
     break;
 }
 }
-*/
+
 $userId = $ig->people->getUserIdForName('tahusunduk');
 $userids = [];
 $userids[] = $userId;
@@ -33,3 +34,23 @@ var_dump($userids);
 $text = "intaannn";
 
 $send = $ig->direct->sendText(["users"=>$userids], $text);
+*/
+$rankToken = \InstagramAPI\Signatures::generateUUID();
+$storyfeed = $ig->story->getReelsTrayFeed($rankToken);
+/////////////////////////////////////////////
+
+
+/*
+$tray = $storyfeed->getTray();
+$json = ($tray[0]->getItems());
+$id = $json[0]->getId();
+echo $id;
+*/
+$tray = $storyfeed->getTray();
+foreach($tray as $tai){
+foreach($tai->getItems() as $cok){
+    $anu =($cok->getId());
+    $delok = $ig->story->markMediaSeen(["item"=>$cok]);
+    echo $delok . "\n" .$cok . "\n";
+}
+}
